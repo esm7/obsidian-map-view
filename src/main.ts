ba75beee123c123a2bf268073b6a078fa0c679df
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-05-02 11:48:38 +0100

    Improved regex parsing
    The old regex relied on positional outputs which could get complex to maintain.
    This changes the regex to use named capture groups.
    This makes everything more explicit and easier to read.
    Improves numerical captures to only capture valid numbers.

--- src/main.ts
+++ src/main.ts
@@ -341,8 +341,8 @@ export default class MapViewPlugin extends Plugin {
         let selectedLocation = null;
         if (match)
             selectedLocation = new leaflet.LatLng(
-                parseFloat(match[2]),
-                parseFloat(match[3])
+                parseFloat(match.groups.lat),
+                parseFloat(match.groups.long)
             );
         else {
             const fmLocation = getFrontMatterLocation(view.file, this.app);
