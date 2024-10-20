3085a3edd93ca69ff8bb5040f8bd1c83dc5773d0
Noah Dunbar <thenoahbz@gmail.com>
2024-10-19 23:39:43 -0500

    fix: auto theme not following Obsidian theme (#284)
    Closes #136 and #274

--- src/mapContainer.ts
+++ src/mapContainer.ts
@@ -220,9 +220,14 @@ export class MapContainer {
     isDarkMode(settings: PluginSettings): boolean {
         if (settings.chosenMapMode === 'dark') return true;
         if (settings.chosenMapMode === 'light') return false;
+
         // Auto mode - check if the theme is dark
-        if ((this.app.vault as any).getConfig('theme') === 'obsidian')
-            return true;
+        const theme = (this.app.vault as any).getConfig('theme');
+        if (theme === 'obsidian') return true;
+        else if (theme === 'moonstone') return false;
+        else if (theme === 'system')
+            return !document.body.classList.contains('theme-light');
+
         return false;
     }
 
